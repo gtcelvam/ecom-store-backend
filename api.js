c52dac9ff6d1eff9aa9360f2ee24620277b4d534
gtcelvam <selvasizzler@gmail.com>
2024-08-10 22:08:17 +0530

    netlify initial configs added.

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 const cors = require("cors");
 
@@ -12,8 +13,10 @@ app.use(express.json());
 app.use(cors());
 
 //routes
-app.use("/api", ProductRoute);
+app.use("/.netlify/functions/api", ProductRoute);
 
 app.listen(PORT_NUM, () => {
   console.log("Server connected successfully!!!");
 });
+
+module.exports.handler = serverless(app);
